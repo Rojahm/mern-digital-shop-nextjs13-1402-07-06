313ebec571198e1451e80c1caedf3a0c787e3710
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-09-30 17:42:10 +0330

    Update product Slider

--- app/components/slider/ProductBox.jsx
+++ app/components/slider/ProductBox.jsx
@@ -7,17 +7,17 @@ import { BiBookmark } from "react-icons/bi";
 
 function ProductBox() {
   return (
-    <article className="slider-item bg-stone-200 w-64 transition-all ease-in-out duration-300 shadow-[0px_1px_10px_rgba(0,0,0,0.15)] hover:shadow-[0px_1px_10px_rgba(0,0,0,0.25)] rounded-lg h-72 min-w-[238px]">
+    <article className="relative slider-item bg-stone-200 w-64 transition-all ease-in-out duration-300 shadow-[0px_1px_10px_rgba(0,0,0,0.15)] hover:shadow-[0px_1px_10px_rgba(0,0,0,0.25)] rounded-lg h-80 min-w-[238px]">
       <Link href={"/"}>
         <Image
           src={"/images/product/ganj.jpg"}
-          width={260}
-          height={140}
+          width={280}
+          height={180}
           objectFit="cover"
           layout="fixed"
           className="rounded-t-md mb-4 my-0 mx-auto"
         />
-        <h3 className="font-bold text-stone-800 text-sm pr-3">
+        <h3 className="font-bold text-stone-600 text-sm pr-3">
           کتاب گنج - نوشته جلال آل احمد کتاب گنج - نوشته جلال آل احمد
         </h3>
       </Link>
@@ -35,7 +35,7 @@ function ProductBox() {
         </Link>
       </div>
       {/* navigation buttons */}
-      <div className="flex flex-row-reverse justify-between h-10 mt-6">
+      <div className="absolute right-0 left-0 bottom-2 flex flex-row-reverse justify-between items-end mt-6">
         <span className="flex flex-row-reverse gap-1">
           <span className="bg-stone-500 align-center p-2 px-3 text-sm text-stone-100 rounded-tr-md rounded-br-md">
             2000 تومان
