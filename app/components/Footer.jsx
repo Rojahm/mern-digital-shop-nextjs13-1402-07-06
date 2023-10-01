5123c52b9b4fe56ed58bb4d129ff84968d5870c4
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-10-01 22:07:15 +0330

    footer basis corrected and licenses added

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -3,44 +3,49 @@ import Link from "next/link";
 
 function Footer() {
   return (
-    <footer className="mt-14 mb-5 text-stone-500">
-      <div className="flex justify-between items-start gap-5 w-[95%] my-0 mx-auto bg-stone-100 p-5 rounded-md">
-        <div className="basis-2/5">
-          <Link
-            href={"/"}
-            className="p-3 px-14 flex flex-col items-center justify-around"
-          >
+    <footer className="mt-14 mb-5">
+      <div className="flex justify-between items-center gap-5 w-[95%] my-0 mx-auto bg-stone-100 text-stone-500 p-5 rounded-md">
+        <div className="basis-1/3">
+          <Link href={"/"} className="p-3 flex flex-col items-center">
             <Image src={"/trans-icon.png"} width={100} height={100} />
-            <p className="mt-2 text-center sm:text-md md:text-sm lg:text-md">
+            <p className="mt-2 text-center sm:text-md md:text-sm lg:text-md px-14">
               لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
               استفاده از
             </p>
           </Link>
         </div>
-        <div className="basis-1/5 mt-5">
-          <h3 className="text-lg mb-4">دسترسی سریع</h3>
-          <ul className="list-disc pr-4">
-            <li className="sm:text-md md:text-sm lg:text-md">درباره ما</li>
-            <li className="sm:text-md md:text-sm lg:text-md">وبلاگ</li>
-            <li className="sm:text-md md:text-sm lg:text-md">حریم خصوصی</li>
-            <li className="sm:text-md md:text-sm lg:text-md">تماس با ما</li>
-          </ul>
+        <div className="basis-1/3 flex gap-7">
+          <div>
+            <h3 className="text-lg mb-4">دسترسی سریع</h3>
+            <ul className="list-disc pr-4">
+              <li className="sm:text-md md:text-sm lg:text-md">درباره ما</li>
+              <li className="sm:text-md md:text-sm lg:text-md">وبلاگ</li>
+              <li className="sm:text-md md:text-sm lg:text-md">حریم خصوصی</li>
+              <li className="sm:text-md md:text-sm lg:text-md">تماس با ما</li>
+            </ul>
+          </div>
+          <div>
+            <h3 className="text-lg mb-4">راهنمای خرید</h3>
+            <ul className="list-disc pr-4">
+              <li className="sm:text-md md:text-sm lg:text-md">
+                سوالات متداول
+              </li>
+              <li className="sm:text-md md:text-sm lg:text-md">
+                چگونه خرید کنم؟
+              </li>
+              <li className="sm:text-md md:text-sm lg:text-md">
+                قوانین استفاده از محصولات
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="basis-1/5 mt-5">
-          <h3 className="text-lg mb-4">راهنمای خرید</h3>
-          <ul className="list-disc pr-4">
-            <li className="sm:text-md md:text-sm lg:text-md">سوالات متداول</li>
-            <li className="sm:text-md md:text-sm lg:text-md">
-              چگونه خرید کنم؟
-            </li>
-            <li className="sm:text-md md:text-sm lg:text-md">
-              قوانین استفاده از محصولات
-            </li>
-          </ul>
-        </div>
-        <div className="basis-1/5 flex justify-between gap-2">
-          <div>4</div>
-          <div>5</div>
+        <div className="basis-1/3 flex justify-center self-center gap-2">
+          <div>
+            <Image src={"/images/Licenses/1.png"} width={100} height={100} />
+          </div>
+          <div>
+            <Image src={"/images/Licenses/2.png"} width={100} height={100} />
+          </div>
         </div>
       </div>
     </footer>
