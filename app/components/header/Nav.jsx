ad719b415867a3c510dbe7956810df47cc405623
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-10-03 23:16:24 +0330

    menu for lg screen

--- app/components/header/Nav.jsx
+++ app/components/header/Nav.jsx
@@ -1,6 +1,6 @@
 function Nav() {
   return (
-    <div className="flex gap-3">
+    <div className="flex gap-3 w-full">
       <div className="menu-btn">صفحه اصلی</div>
       <div className="menu-btn">بازی</div>
       <div className="menu-btn">طرح</div>
