fff2c1ef3fd2de89c19ffa05d9b496d57e68aac5
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-10-13 16:22:28 +0330

    go to top button

--- app/components/Credit.jsx
+++ app/components/Credit.jsx
@@ -1,11 +1,21 @@
+"use client";
 //ui
 import { FaArrowUp } from "react-icons/fa";
 
 function Credit() {
+  const goTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
   return (
     <div className="container px-5 w-[95%] flex justify-between pb-2">
       <h6>نمیتبمنسیبسمین</h6>
-      <button className="bg-yellow-400 p-2 rounded-md shadow-md hover:shadow-lg hover:-translate-y-2 transform-all ease-in-out duration-300">
+      <button
+        onClick={goTop}
+        className="bg-yellow-400 p-2 rounded-md shadow-md hover:shadow-lg hover:-translate-y-2 transform-all ease-in-out duration-300"
+      >
         <FaArrowUp />
       </button>
     </div>
