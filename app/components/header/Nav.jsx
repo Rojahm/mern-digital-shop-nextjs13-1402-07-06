b4bed81b6f1fe593443a1ae33eee7bc4092bff39
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-10-14 00:01:14 +0330

    blog link unpdated

--- app/components/header/Nav.jsx
+++ app/components/header/Nav.jsx
@@ -1,10 +1,14 @@
+import Link from "next/link";
+
 function Nav() {
   return (
     <div className="flex gap-3 w-full">
       <div className="menu-btn">صفحه اصلی</div>
       <div className="menu-btn">بازی</div>
       <div className="menu-btn">طرح</div>
-      <div className="menu-btn">بلاگ</div>
+      <div className="menu-btn">
+        <Link href={"/blog"}>بلاگ</Link>
+      </div>
     </div>
   );
 }
