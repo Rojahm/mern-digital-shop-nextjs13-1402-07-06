ff0ab202c88d969a30de408ebfffd1cff5a8562a
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-10-14 21:53:20 +0330

    to the top button fixed on footer

--- app/components/Credit/index.jsx
+++ app/components/Credit/index.jsx
@@ -10,11 +10,11 @@ function Credit() {
     });
   };
   return (
-    <div className="container px-5 w-[95%] flex justify-between pb-2">
+    <div className="container relative px-5 w-[95%] flex justify-between pb-2">
       <h6>نمیتبمنسیبسمین</h6>
       <button
         onClick={goTop}
-        className="bg-yellow-400 p-2 rounded-md shadow-md hover:shadow-lg hover:-translate-y-2 transform-all ease-in-out duration-300"
+        className="fixed bottom-3 left-10 bg-yellow-400/50 hover:bg-yellow-400 text-stone-600 p-2 rounded-md shadow-md hover:shadow-lg hover:-translate-y-2 transform-all ease-in-out duration-300"
       >
         <FaArrowUp />
       </button>
