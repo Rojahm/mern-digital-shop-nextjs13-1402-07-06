4387f69bb4afe83f28d0e39c59b43ba4f32ec83f
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-10-28 18:15:07 +0330

    changed http method

--- server/routes/MiddleBannerRoutes.js
+++ server/routes/MiddleBannerRoutes.js
@@ -4,6 +4,6 @@ const router = express();
 const MiddleBannerCtrl = require("../controllers/MiddleBannerCtrl");
 
 router.get("/middle-banners", MiddleBannerCtrl.getAllMiddleBanners);
-router.get("/new-middle-banner", MiddleBannerCtrl.newMiddleBanner);
+router.post("/new-middle-banner", MiddleBannerCtrl.newMiddleBanner);
 
 module.exports = router;
