0f9ad358903e6b988f52bf401df7564a22778397
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-10-28 16:59:59 +0330

    middle banner model

---
+++ server/models/MiddleBanner.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const MiddleBanner = new mongoose.Schema({
+  image: {
+    required: true,
+    type: String,
+  },
+  imageAlt: {
+    required: true,
+    type: String,
+  },
+  mode: {
+    required: true,
+    type: Boolean,
+  },
+  link: {
+    required: true,
+    type: String,
+  },
+  publishDate: {
+    type: String,
+    default: new Date().toLocaleDateString("fa-IR", {
+      hour: "2-digit",
+      minute: "2-digit",
+    }),
+  },
+});
+module.exports = mongoose.model("MiddleBanner", MiddleBanner);
