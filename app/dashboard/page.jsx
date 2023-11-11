469bc92eaa627ec1559590a05ce5c63e8bb0baef
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-11-11 20:53:42 +0330

    dashboard

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,12 @@
+import DashboardCtrl from "@/components/dashboard/dashboard-ctrl";
+
+function DashboardPage() {
+  return (
+    <div>
+      <h1>dashboard page</h1>
+      <DashboardCtrl />
+    </div>
+  );
+}
+
+export default DashboardPage;
---
+++ components/dashboard/dashboard-ctrl/index.jsx
@@ -0,0 +1,9 @@
+function DashboardCtrl() {
+  return (
+    <div>
+      <h1>button</h1>
+    </div>
+  );
+}
+
+export default DashboardCtrl;
