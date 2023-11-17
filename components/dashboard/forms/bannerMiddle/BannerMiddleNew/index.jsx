0e4de86acdfb6c1879bdfdfa661ff90d587d184b
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-11-17 21:21:04 +0330

    new middle banner form

--- components/dashboard/forms/bannerMiddle/BannerMiddleNew/index.jsx
+++ components/dashboard/forms/bannerMiddle/BannerMiddleNew/index.jsx
@@ -2,6 +2,16 @@ const BannerMiddleNew = () => {
   return (
     <div>
       <h1>Create new Middle Banner</h1>
+      <form className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label for="image-name">photo name</label>
+          <input type="text" id="image-name" />
+        </div>
+        <div className="flex gap-4">
+          <label for="image-name">photo name</label>
+          <input type="text" id="image-name" />
+        </div>
+      </form>
     </div>
   );
 };
