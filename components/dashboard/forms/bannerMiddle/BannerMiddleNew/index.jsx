79751d885edd53afacf5ebc4e55a93e4b4e82b4e
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-11-19 19:04:36 +0330

    new middle banner form

--- components/dashboard/forms/bannerMiddle/BannerMiddleNew/index.jsx
+++ components/dashboard/forms/bannerMiddle/BannerMiddleNew/index.jsx
@@ -1,15 +1,48 @@
 const BannerMiddleNew = () => {
   return (
-    <div>
-      <h1>Create new Middle Banner</h1>
+    <div className="flex flex-col gap-6 mt-5">
+      <h1>بنر میانی جدید</h1>
       <form className="flex flex-col gap-4">
-        <div className="flex gap-4">
-          <label for="image-name">photo name</label>
-          <input type="text" id="image-name" />
+        <div className="flex justify-between gap-2 items-center">
+          <label for="image-name" className="w-1/5 text-center">
+            نام عکس:
+          </label>
+          <input
+            type="text"
+            id="image-name"
+            autoFocus
+            className="p-2 rounded-md w-4/5 border-2 border-zinc-300 focus-visible:outline-orange-500"
+          />
         </div>
-        <div className="flex gap-4">
-          <label for="image-name">photo name</label>
-          <input type="text" id="image-name" />
+        <div className="flex justify-between gap-2 items-center">
+          <label for="image-name" className="w-1/5 text-center">
+            alt عکس:
+          </label>
+          <input
+            type="text"
+            id="image-name"
+            className="p-2 rounded-md w-4/5 border-2 border-zinc-300 focus-visible:outline-orange-500"
+          />
+        </div>
+        <div className="flex justify-between gap-2 items-center">
+          <label for="image-name" className="w-1/5 text-center">
+            لینک عکس:
+          </label>
+          <input
+            type="text"
+            id="image-name"
+            className="p-2 rounded-md w-4/5 border-2 border-zinc-300 focus-visible:outline-orange-500"
+          />
+        </div>
+        <div className="flex justify-between gap-2 items-center">
+          <label for="image-name" className="w-1/5 text-center">
+            روشن / خاموش :
+          </label>
+          <input
+            type="text"
+            id="image-name"
+            className="p-2 rounded-md w-4/5 border-2 border-zinc-300 focus-visible:outline-orange-500"
+          />
         </div>
       </form>
     </div>
